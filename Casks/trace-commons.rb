17032a49684daa17f3ffb1055fef8ce19d911a7a
Zaki Manian <zaki@iqlusion.io>
2026-08-23 19:44:33 +0100

    trace-commons 0.4.7 (#19)
    * trace-commons 0.4.7
    
    * Correct the 0.4.7 DMG checksum after the release was rebuilt
    
    ---------
    
    Co-authored-by: trace-commons-release <ops@tracecommons.ai>

--- Casks/trace-commons.rb
+++ Casks/trace-commons.rb
@@ -1,6 +1,6 @@
 cask "trace-commons" do
-  version "0.4.0"
-  sha256 "22dc77041c5d4f4125e5f17a3e3df0a2416f1132cca208b1209334e1f4cca037"
+  version "0.4.7"
+  sha256 "d60ce434da359ad562e9ddbf8d4b97ddd665b2aede47d3c1d188d4894b4c3b2e"
 
   url "https://github.com/TraceCommons/trace-commons-server/releases/download/app-v#{version}/TraceCommons-#{version}.dmg"
   name "Trace Commons"
