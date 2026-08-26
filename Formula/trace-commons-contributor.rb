81f1600b8b6bd86555a998b864d68c0a557a28c7
trace-commons-release <ops@tracecommons.ai>
2026-08-26 10:39:38 +0000

    trace-commons-contributor 0.5.0

--- Formula/trace-commons-contributor.rb
+++ Formula/trace-commons-contributor.rb
@@ -1,17 +1,17 @@
 class TraceCommonsContributor < Formula
   desc "CLI for contributing coding session traces to the Trace Commons corpus"
   homepage "https://tracecommons.ai/"
-  version "0.4.9"
+  version "0.5.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/TraceCommons/trace-commons-server/releases/download/contributor-v#{version}/trace-commons-contributor-aarch64-apple-darwin.zip"
-      sha256 "794584793e90f1fb36a3c813a537da2cf253ec6883534739e0fb30bc8922bc11"
+      sha256 "93cf619ec80a02318b7d83cc936dc2021352efad0c8f41b4eda926d3e1565e41"
     end
     on_intel do
       url "https://github.com/TraceCommons/trace-commons-server/releases/download/contributor-v#{version}/trace-commons-contributor-x86_64-apple-darwin.zip"
-      sha256 "b08f16cd021abb62abc19e5de8b122c9296252bb098972a432bdfc1352c1910b"
+      sha256 "1345da5c2c73af42a6b9749d15dc26425cf542c27d6c39ea09ffce2e6c458033"
     end
   end
 
